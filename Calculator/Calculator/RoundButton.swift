ac8f88ee18560b4cd79e46b44ddcbd51de7921e8
yanJuicy <hanjub3@gmail.com>
2023-11-11 03:57:23 +0900

    계산기 UI 그리기

---
+++ Calculator/Calculator/RoundButton.swift
@@ -0,0 +1,21 @@
+//
+//  RoundButton.swift
+//  Calculator
+//
+//  Created by lyanju on 2023/11/11.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+
+    @IBInspectable var isRound: Bool = false {
+        didSet {
+            if isRound {
+                self.layer.cornerRadius = self.frame.height / 2
+            }
+        }
+    }
+
+}
